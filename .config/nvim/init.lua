daa97c93baae1763602d31c8934258de04f05fe1
absenth <1115138+absenth@users.noreply.github.com>
2023-12-22 23:26:09 -0600

    Fixed Treesitter-context config

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -128,7 +128,7 @@ require('lazy').setup({
     opts = {
       options = {
         icons_enabled = false,
-        theme = 'onedark',
+        theme = 'catppuccin',
         component_separators = '|',
         section_separators = '',
       },
