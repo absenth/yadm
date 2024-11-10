70376ca38b1463ed2474dc66ddb1e774ddcb13ed
absenth <1115138+absenth@users.noreply.github.com>
2024-11-10 09:42:25 -0600

    Setting up SOTD nvim plugin

---
+++ .config/nvim/lua/custom/plugins/sotd.lua
@@ -0,0 +1,23 @@
+return
+{
+    "snooting/sotd.nvim",
+    dependencies = {
+        "nvim-telescope/telescope.nvim",
+        "nvim-lua/plenary.nvim",
+        "b0o/schemastore.nvim",
+    },
+    config = function()
+        require("sotd").setup({
+            -- Optional: override default configuration
+            den_file = vim.fn.expand("~/.config/nvim/den.json"),
+            log_file = vim.fn.expand("~/.config/nvim/sotd.log"),
+            logging_enabled = false,
+            preshave_number = 1,
+            post_number = 4,
+        })
+    end,
+    -- Optional: Add keymaps
+    keys = {
+        { "<leader>sc", "<cmd>SOTDCreate<cr>", desc = "Create SOTD Post" },
+    },
+}
