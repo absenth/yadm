d573c43b58f05fb203e509ecd37e96ed7c7a51fc
absenth <1115138+absenth@users.noreply.github.com>
2025-07-06 21:24:49 -0500

    updating nvim stuff

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1036,10 +1036,11 @@ vim.api.nvim_create_autocmd({ 'BufWritePre' }, {
 -----------------
 
 -- Better window navigation --
-vim.keymap.set('n', '<C-h>', '<C-w>h', opts)
-vim.keymap.set('n', '<C-j>', '<C-w>j', opts)
-vim.keymap.set('n', '<C-k>', '<C-w>k', opts)
-vim.keymap.set('n', '<C-l>', '<C-w>l', opts)
+-- Maybe Fix Me - vim-tmux-navigator testing
+-- vim.keymap.set('n', '<C-h>', '<C-w>h', opts)
+-- vim.keymap.set('n', '<C-j>', '<C-w>j', opts)
+-- vim.keymap.set('n', '<C-k>', '<C-w>k', opts)
+-- vim.keymap.set('n', '<C-l>', '<C-w>l', opts)
 
 -- Resize with arrows --
 vim.keymap.set('n', '<C-Up>', ':resize -2<CR>', opts)
